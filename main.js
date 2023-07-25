d937d8999e6ee5146a049160283ebc6942055baa
Kunal Upadhyay <81215146+Kunal-Upadhyay@users.noreply.github.com>
2023-07-25 23:54:52 +0530

    JS update
    - working courses button
    - working courses radio button using URL parameters

--- main.js
+++ main.js
@@ -4,3 +4,18 @@ window.addEventListener('scroll',function() {
     let offset=window.scrollY;
     MPLO.style.opacity=(400-offset)*.003;
 })
+
+function gtco(button){
+  button.classList.add("active");
+  
+  setTimeout(function() { button.classList.remove("active");}, 700);
+
+  setTimeout(function() {
+  if(button.name == "c")
+    window.location.href = "courses.html?icourse=c";
+  else if(button.name == "cpp")
+    window.location.href = "courses.html?icourse=cpp";
+  else if(button.name == "python")
+    window.location.href = "courses.html?icourse=python";
+  },1000)
+}
